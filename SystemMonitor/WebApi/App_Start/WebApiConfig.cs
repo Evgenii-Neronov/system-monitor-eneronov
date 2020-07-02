e2e9b33faabc4aa180b158892ed4672ed991dead
ueberprogrammer <ueberprogrammer@gmail.com>
2020-07-02 15:30:17 +0300

    remove not needs initializations

--- SystemMonitor/WebApi/App_Start/WebApiConfig.cs
+++ SystemMonitor/WebApi/App_Start/WebApiConfig.cs
@@ -12,12 +12,6 @@ namespace WebApi
         {
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
             config.Formatters.JsonFormatter.SupportedMediaTypes
                 .Add(new MediaTypeHeaderValue("text/html"));
         }
